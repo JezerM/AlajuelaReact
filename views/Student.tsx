154eb408085ef7d2a320e9e9163ecdb4c233e2e6
Jezer Mejía <amyuki4@icloud.com>
2023-11-16 01:26:57 -0600

    Added basic navigation

---
+++ views/Student.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function StudentScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>¡Estudiante!</Text>
+    </View>
+  );
+}
