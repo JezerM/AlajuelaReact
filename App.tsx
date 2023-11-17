0f50d768f2fcfadedeae0ea03968706dc8ea6513
Jezer Mejía <amyuki4@icloud.com>
2023-11-16 21:07:48 -0600

    Improved styles

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "./views/Home";
 import { StudentScreen } from "./views/Student";
 import { SchoolScreen } from "./views/School";
+import styles from "./lib/styles";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Inicio" component={HomeScreen} />
+      <StatusBar barStyle="light-content" />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: styles.primary.color,
+          headerTintColor: "#FFFFFF",
+          headerStyle: {
+            backgroundColor: styles.primary.color,
+          },
+        }}>
+        <Tab.Screen
+          name="Inicio"
+          options={{ title: "San Rafael de Alajuela" }}
+          component={HomeScreen}
+        />
         <Tab.Screen name="Estudiante" component={StudentScreen} />
         <Tab.Screen name="Escuela" component={SchoolScreen} />
       </Tab.Navigator>
---
+++ lib/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  primary: {
+    color: "#1F97F4",
+  },
+});
+
+export default styles;
