9a9bc27b4a669523f34351e3ba6d1800b5dcf87c
Jezer Mejía <amyuki4@icloud.com>
2023-11-17 14:41:07 -0600

    Added logo image in Home

--- views/Home.tsx
+++ views/Home.tsx
@@ -1,8 +1,21 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}>
+      <Image
+        source={require("../assets/logo-alajuela.jpg")}
+        style={{
+          aspectRatio: 3 / 4,
+          height: 200,
+        }}
+      />
       <Text>¡Inicio!</Text>
     </View>
   );
